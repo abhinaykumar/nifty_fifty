2f13af83c4861b80a39aefe975ef9a71eb5ffe00
Aks <abhinay302@gmail.com>
2017-05-18 19:27:31 +0530

    some refactoring

--- update_redis.py
+++ update_redis.py
@@ -3,7 +3,6 @@ import redis
 import time
 
 r = redis.StrictRedis(host='localhost', port=6379)
-systime = time.time()
 
 def nifty_list():
     response = requests.get('https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json')
