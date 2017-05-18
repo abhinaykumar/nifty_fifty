0abee9be2f1e5c288646df67fac0101f0b02408e
Aks <abhinay302@gmail.com>
2017-05-18 18:36:18 +0530

    Add more values

--- views/js/nifty.js
+++ views/js/nifty.js
@@ -34,7 +34,11 @@ function update_list(data) {
       var card = '<div class="col-xs-3"><div class="project project-'+ projectClass +'"><div class="shape"><div class="shape-text">'+
                   jdata[key]["symbol"] + '</div></div><div class="project-content"><h3 class="lead">' +
                   jdata[key]["symbol"] + '</h3><ul style="list-style-type:lower-greek"><li><b>Open Price:</b> '+ jdata[key]["openPrice"] +'</li><li><b>High Price:</b> '+
-                  jdata[key]["highPrice"]+'</li><li><b>Low Price:</b> '+ jdata[key]["lowPrice"] +'</li><li><b>Traded Quantity:</b> '+ jdata[key]["tradedQuantity"] +'</li></ul></div></div></div>';
+                  jdata[key]["highPrice"]+'</li><li><b>Low Price:</b> '+ jdata[key]["lowPrice"] +'</li><li><b>Traded Quantity:</b> '+
+                  jdata[key]["tradedQuantity"]+'</li><li><b>LTP:</b> '+ jdata[key]["ltp"] +'</li><li><b>Previous Price:</b> '+
+                  jdata[key]["previousPrice"] + '</li><li><b>Net Price:</b> '+ jdata[key]["netPrice"]+'</li><li><b>Turnover(In lakhs):</b> '+
+                  jdata[key]["turnoverInLakhs"]+ '</li><li><b>Last Corp Announcement Date:</b> '+ jdata[key]["lastCorpAnnouncementDate"]+
+                  '</li><li><b>Last Corp Announcement:</b> '+ jdata[key]["lastCorpAnnouncement"]+'</li></ul></div></div></div>';
 
       $("#stockList").append(card);
       var a = JSON.stringify(companyDetails);
