b563c81fcfe277ad4f29e73aac006f87922f5956
Aks <abhinay302@gmail.com>
2017-05-18 16:18:03 +0530

    Add cards to display info

--- nifty_fifty.py
+++ nifty_fifty.py
@@ -3,6 +3,7 @@ import requests
 from os import path, curdir
 import redis
 import json
+import ast
 
 class NiftyFifty(object):
 	@cherrypy.expose
@@ -10,19 +11,29 @@ class NiftyFifty(object):
 		return file("views/nifty/index.html")
 
 	@cherrypy.expose
-	def new
-		response = requests.get('https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json')
-		j = response.text.encode('utf-8')
-		js = json.dumps(j)
-		print type(js)
+	def show(self, _):
 		r = redis.StrictRedis(host='localhost', port=6379)
-		json_response = json.dumps(response.text)
-		r.set('nifty', json_response)
-		get_redis= json.loads(r.get('nifty'))
+		nifty_list = r.get('nifty')
+		json_response = json.dumps(nifty_list)
+		return json_response
 
-# cherrypy.config.update({'server.socket_host': '0.0.0.0',
-#                         'server.socket_port': 8080,
-#                        })
+	@cherrypy.expose
+	def new(self, _):
+		r = redis.StrictRedis(host='localhost', port=6379)
+		response = requests.get('https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json')
+		r.set('nifty', response.text)
+		return None
+
+	@cherrypy.expose
+	def logo(self, _, symbol):
+		company_details = requests.get("https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/companySnapshot/getContactDetails"+symbol+".json")
+		dict_reponse = ast.literal_eval(company_details.text)
+		web_address = dict_reponse["rows"][0]["webAddress"]
+		return web_address
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 8080,
+                       })
 cherrypy.quickstart(NiftyFifty(), "/", { "/views": {
                         "tools.staticdir.on": True,
                         "tools.staticdir.dir" : path.join(path.abspath(curdir), "views")}})
---
+++ views/js/nifty.js
@@ -0,0 +1,55 @@
+function displayList() {
+  $.get('/show?_=' + new Date().getTime(), function() {
+  })
+    .done(function (data) {
+        update_list(data);
+    })
+    .always(function () {
+        setTimeout(displayList, 30000);
+    });
+}
+
+function getNiftyData() {
+    $.get('/new?_=' + new Date().getTime(), function(){
+    })
+    .always(function () {
+        setTimeout(getNiftyData, 50000);
+    });
+}
+
+$(document).ready(function () {
+    displayList();
+    getNiftyData();
+})
+
+function update_list(data) {
+    var data = JSON.parse(JSON.parse(data));
+    jdata = data["data"];
+    $("#stockList").html("");
+    var projectClass = 0;
+    for(var key in jdata){
+      var symbol = jdata[key]["symbol"];
+      var companyDetails = getCompanyDetails(symbol);
+      if(projectClass == 5){ projectClass = 0;}
+      var card = '<div class="col-xs-3"><div class="project project-'+ projectClass +'"><div class="shape"><div class="shape-text">'+
+                  jdata[key]["symbol"] + '</div></div><div class="project-content"><h3 class="lead">' +
+                  jdata[key]["symbol"] + '</h3><ul style="list-style-type:lower-greek"><li><b>Open Price:</b> '+ jdata[key]["openPrice"] +'</li><li><b>High Price:</b> '+
+                  jdata[key]["highPrice"]+'</li><li><b>Low Price:</b> '+ jdata[key]["lowPrice"] +'</li><li><b>Traded Quantity:</b> '+ jdata[key]["tradedQuantity"] +'</li></ul></div></div></div>';
+
+      $("#stockList").append(card);
+      var a = JSON.stringify(companyDetails);
+      console.log(a["responseText"]);
+      projectClass += 1;
+    }
+    $("#time").html(data["time"])
+}
+
+function getCompanyDetails(symbol){
+  var details = $.ajax({
+      url: "/logo?_="+ new Date().getTime(),
+      data: {
+          "symbol": symbol
+      }
+  });
+  return details;
+}
