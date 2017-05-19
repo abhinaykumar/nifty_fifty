64bda4c761d99e66ac4a4f930af49ec8505537ab
Aks <abhinay302@gmail.com>
2017-05-19 14:06:19 +0530

    code refactoring

--- update_redis.py
+++ update_redis.py
@@ -1,16 +1,15 @@
 import requests
 import redis
 import time
-import cherrypy
 
 r = redis.StrictRedis(host='localhost', port=6379)
-class NiftyFifty(object):
-    def nifty_list():
-        response = requests.get('https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json')
-        r.set('nifty', response.text)
-        return None
+
+def nifty_list():
+    response = requests.get('https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json')
+    r.set('nifty', response.text)
+    return None
 
 if __name__ == "__main__":
-    cherrypy.quickstart(NiftyFifty())
-    nifty_list()
+    while True:
+        nifty_list()
         time.sleep(300)
